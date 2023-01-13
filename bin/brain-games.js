45f5b7cb4cbcac2ee0e170658d6546383a622807
Ilya Mikhaylyukov <ilya_mikhaylyukov@MacBook-Pro-Ilya.local>
2023-01-13 13:47:22 +0700

    инициализация

---
+++ bin/brain-games.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const showGreeting = () => {
+  // Внутри тела отступ 2 пробела для удобства чтения
+  const text = 'Welcome to the Brain Games!';
+  console.log(text);
+};
+
+// Вызов функции
+showGreeting();
